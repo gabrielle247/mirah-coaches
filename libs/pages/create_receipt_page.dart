9bd5d3cfd463d13e63639aada11db40c1d41e8d7
MrGee <133508930+gabrielle247@users.noreply.github.com>
2025-11-17 17:13:53 +0200

    Add CreateReceiptPage widget with placeholder

--- libs/pages/create_receipt_page.dart
+++ libs/pages/create_receipt_page.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class CreateReceiptPage extends StatelessWidget {
+  const CreateReceiptPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
