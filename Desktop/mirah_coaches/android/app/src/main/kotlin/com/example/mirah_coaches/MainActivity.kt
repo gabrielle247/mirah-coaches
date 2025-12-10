8fe1644a618e2650ab186d0dbfa45e7a643d9c34
Gabriel Tawananyasha Kuudzadombo <133508930+gabrielle247@users.noreply.github.com>
2025-12-10 08:26:11 +0200

    Delete Desktop/mirah_coaches directory

--- Desktop/mirah_coaches/android/app/src/main/kotlin/com/example/mirah_coaches/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mirah_coaches
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Desktop/mirah_coaches/lib/utils/tabs/tab_imports.dart
+++
@@ -1,4 +0,0 @@
-export 'dashboard_tab.dart';
-export 'expenses_tab.dart';
-export 'receipts_tab.dart';
-export 'ticket_tab.dart';
