1713b68a1a6430e0b3fbd39e950f4e66b357db50
Gabriel Tawananyasha Kuudzadombo <133508930+gabrielle247@users.noreply.github.com>
2025-12-10 08:27:09 +0200

    Delete Development directory

--- Development/the_plug/android/app/src/main/kotlin/com/example/the_plug/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.the_plug
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Development/updated_fees_up/android/app/src/main/kotlin/com/example/updated_fees_up/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.updated_fees_up
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
